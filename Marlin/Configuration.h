772571b10757abdac2b4003b618c713a13df7f10
Peter Hall <33176108+IamPete1@users.noreply.github.com>
2020-08-21 20:19:13 +0100

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1119,6 +1119,8 @@
 // @section machine
 
 // The size of the print bed
+#define X_BED_SIZE 335
+#define Y_BED_SIZE 250
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
